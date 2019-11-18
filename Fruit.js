d9a014382a4651ab09ecb58eabf73f4dfca3f0a4
Anushka Verma <v.anushka786@gmail.com>
2019-11-18 20:49:18 +0530

    Remove modes

--- Fruit.js
+++ Fruit.js
@@ -1,97 +1,73 @@
 
 function Fruit(x,y,speed,color,size,fruit,bomb){
-
     this.x = x;
     this.y = y;
     this.speed = speed;
     this.color = color;
-    // this.clearCol = clearColor(color); //color with no alpha
-    
     this.bomb = bomb; 
     this.size = size;
     this.xSpeed = randomXSpeed(x);
     this.ySpeed = random(-10.4, -7.4);
     this.fruit = fruit;
-    // this.cSansAlpha = removeAlpha(color); 
     this.sliced = false;
     this.visible = true;
 }
 
+Fruit.prototype.draw = function(){
+    // console.log(fruitsList.length);
+    fill(this.color);
+    if(this.sliced){
+        // if(this.bomb){
+        //     gameOver();
+        // }
+        // this.color = lerpColor(this.color, color(51), 0.5);
+    }else{
+        image(this.fruit, this.x, this.y, this.size, this.size);
+        // console.log(this.fruit);
+        // var b = Math.round(random(99));
+        // if(b % 5 === 0 ){
+        //     image(this.bomb, this.x + 10, this.y - 10, this.size, this.size);
+        //     // // if(this.bomb.visible){
+        //     // //     console.log("visible");
+        //     //     if(bomb.sliced){
+        //     //         console.log("BOMB");
+        //     //         gameOver();
+        //     //     }
+        //     // // }
+        // }
+        if(this.fruit === bomb){
+            console.log("BOMB!!!!!!!!!")
+        }
+    }
+};
+
 Fruit.prototype.update = function(){
 
-    // ySpeed = []
     this.x += this.xSpeed;
     this.y += this.ySpeed;
-
-    // this.xSpeed *= 0.95;
     this.ySpeed += GRAVITY;
-
     if(this.y > height){
         this.visible = false;
     }
 
 };
-Fruit.prototype.draw = function(){
-    // noStroke();
-    fill(this.color);
-    if(this.sliced){
-
-        // if(this.bomb){
-        //     // Game Over
-        //     gameOver();
-        // }
-        // if fruit is sliced, fade it out
-        this.color = lerpColor(this.color, color(51), 0.5); // ??
-    }else{
-        
-        image(this.fruit, this.x, this.y, this.size, this.size);
-        // ellipse(this.x, this.y, this.size)
-        // rect(this.x, this.y, this.size, this.size);
-    }
-    
-};
-
 
 function randomFruit(){
     var x = random(width);
     var y = height;
     var size = noise(frameCount)*20 + 40;
-
-    // var red = noise(frameCount) * 255;
-    // var green = noise(frameCount * 2) * 255;
-    // var blue = noise(frameCount * 3) * 255;
-    // var col = color(red,green,blue);
-
     var col = color(random(255),random(255),random(255));
     var speed = random(3,5);
-
+    var idx = round(random(0,fruitsList.length-1));
+    // console.log(idx);
     var fruit = random(fruitsImgs);
     return new Fruit(x,y,speed,col,size,fruit,bomb);
 }
 
-// function clearColor(col){
-    
-//     var r = red(col);
-//     var g = green(col);
-//     var b = blue(col);
-//     return color(r, g, b, 0);
-// }
-
 function randomXSpeed(x){
     if( x > width/2 ){
-        // If fruit generated on right side, go left
-        return random(-2.8,-0.5);
+        return random(-2.8,-0.5); // If fruit generated on right side, go left
     }else{
-        // If fruit generated on right side, go left
-        return random(0.5,2.8);        
+        return random(0.5,2.8); // If fruit generated on right side, go left  
     }
 };
-
-
-// function removeAlpha(color){
-//     var red = red(color);
-//     var green = green(color);
-//     var blue = blue(color);
-
-//     return (red,green,blue,0);
-// }
---
+++ main.js
@@ -0,0 +1,199 @@
+var cnv;
+var sword;
+var score;
+var lives;
+var x = 0; //for lives
+var button;
+var bomb;
+var fruit = [];
+var livesImgs = [];
+var livesImgs2 = [];
+var fruitsList = ['apple', 'banana', 'peach', 'strawberry', 'watermelon'];
+var fruitsImgs = [];
+var isPlay = false;
+// var timer;
+// var counter = 60;
+// var seconds, minutes;
+var timerValue = 60;
+var startButton;
+
+var GRAVITY = 0.1;
+let img;
+
+function setup(){
+
+    for(var i=0; i<fruitsList.length; i++){
+        fruitsImgs[i] = loadImage('images/'+ fruitsList[i] + '.png');
+    }
+
+    for(var i=0; i<3; i++){
+        livesImgs[i] = loadImage('images/x'+ (i+1) + '.png');
+    }
+
+    for(var i=0; i<3; i++){
+        livesImgs2[i] = loadImage('images/xx'+ (i+1) + '.png');
+    }
+
+    bomb = loadImage('images/boom.png');
+    scoreImg = loadImage('images/score.png');
+    gameOverImg = loadImage('images/game-over.png');
+    logo = loadImage('images/logo.png');
+    fruitImg = loadImage('images/fruitMode.png');
+    bg = loadImage('images/background.jpg');
+    cnv = createCanvas(800,635);
+    sword = new Sword(color("#FFFFFF"));
+    frameRate(60);
+    score = 0;
+    lives = 3;
+    // timer = createP("timer");
+    // setInterval(timeIt, 1000);
+
+    // textAlign(CENTER);
+    // setInterval(timeIt, 1000);
+    
+    //   if (timerValue == 0) {
+    //     text('game over', width / 2, height / 2 + 15);
+    //   }
+    // fruit.push(new Fruit(random(width),height,3,"#FF00FF",random()));
+}
+
+// function resetSketch(){
+//     clear();
+//     background(bg);
+//     game();
+// }
+
+// function timeIt() {
+//     console.log("time");
+//     if (timerValue > 0) {
+//         console.log(timerValue);
+//         timerValue--;
+//         textAlign(CENTER);
+//         noStroke();
+//         fill(255,147,21);
+//         textSize(50);
+//         text(timerValue, 200, 250);
+//     }
+//   }
+
+function draw(){
+    // console.log("draw");
+    clear();
+    background(bg);
+    image(this.logo, 20, 20, 288, 135);
+    textAlign(LEFT);
+    noStroke();
+    fill(255,147,21);
+    textSize(50);
+    image(this.fruitImg, 390, 280, 60, 60);
+    cnv.mouseClicked(check);
+    
+    if(isPlay){
+        game();
+    }
+    // game();
+    // if(startButton){
+    //     if (timerValue >= 60) {
+    //         text("0:" + timerValue, width / 2, height / 2);
+    //     }
+    //     if (timerValue < 60) {
+    //         text('0:0' + timerValue, width / 2, height / 2);
+    //     }
+    // }
+}
+
+function check(){
+    // console.log(mouseX);
+    // console.log(mouseY);
+    if(mouseX > 380 && mouseX < 450 && mouseY > 280 && mouseY < 340){
+        isPlay = true;
+    }
+}
+
+function game(){
+    clear();
+    background(bg);
+    if(mouseIsPressed){
+        sword.swipe(mouseX, mouseY);
+    }
+    
+    if(frameCount % 5 === 0){
+        if(noise(frameCount) > 0.69){
+            fruit.push(randomFruit()); // Display new fruit
+        }
+    }
+    var points = 0;
+    for(var i=fruit.length-1; i>=0; i--){
+        fruit[i].update();
+        fruit[i].draw();
+        if(!fruit[i].visible){
+            if(!fruit[i].sliced){
+                lives--;
+                if(lives == 2){
+                    image(this.livesImgs2[0], width - (3*30 + 30), 20, livesImgs2[0].width, livesImgs2[0].height);
+                }
+                x++;
+            }else{
+                // if( fruitsList[i] === 'boom'){
+                //     console.log( fruitsList[i] );
+                //     console.log("boom!!!");
+                //     gameOver();
+                //     // draw();
+                // }
+            }
+            if(lives < 1){
+                gameOver();
+            }
+            fruit.splice(i,1);
+        }else{
+            points += (sword.checkSlice(fruit[i])) ? 1 : 0;
+        }
+    }
+    if(frameCount % 2 === 0 ){
+        sword.update();
+    }
+    sword.draw();
+    score += points;
+    drawScore();
+    drawLives();  
+}
+
+function drawLives(){
+    stroke(255);
+    strokeWeight(3);
+    fill("#FF00EE");
+    image(this.livesImgs[0], width - 110, 20, livesImgs[0].width, livesImgs[0].height);
+    image(this.livesImgs[1], width - 88, 20, livesImgs[1].width, livesImgs[1].height);
+    image(this.livesImgs[2], width - 60, 20, livesImgs[2].width, livesImgs[2].height);
+    
+    if(lives <= 2 ){
+        image(this.livesImgs2[0], width - 110, 20, livesImgs2[0].width, livesImgs2[0].height);
+    }
+    if(lives <= 1){
+        image(this.livesImgs2[1], width - 88, 20, livesImgs2[1].width, livesImgs2[1].height);
+    }
+    if(lives === 0){
+        image(this.livesImgs2[2], width - 60, 20, livesImgs2[2].width, livesImgs2[2].height);
+    }
+}
+
+function drawScore(){
+    image(this.scoreImg, 10, 10, 40, 40);
+    textAlign(LEFT);
+    noStroke();
+    fill(255,147,21);
+    textSize(50);
+    text(score, 50, 50);
+}
+
+function gameOver(){
+    noLoop();
+    clear();
+    background(bg);
+    image(this.gameOverImg, 155, 260, 490, 85);
+    lives = 0;
+    // button = createButton("Reset");
+    // button.position(450, 350);
+    // button.mousePressed(resetSketch);
+    console.log("lost");
+}
